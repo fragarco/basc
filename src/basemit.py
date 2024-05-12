65d8331f07b72bcf6426edbe1e7dab26a25c96c3
Javier Garcia <fragarco@gmail.com>
2024-05-12 20:41:08 +0200

    simple boilerplate to develop de emitter

--- src/basemit.py
+++ src/basemit.py
@@ -14,7 +14,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 """
-
+import sys
 from bastypes import Symbol, SymbolTable, Expression, BASTypes
 
 class ISMEmitter:
@@ -23,6 +23,33 @@ class ISMEmitter:
     """
 
     def __init__(self):
-        self.icode = []
+        self.code = []
 
- 
+    def abort(self, message):
+        print(message)
+        sys.exit(1)
+
+    def load(self, value):
+        self.code.append(f'LOAD {value}')
+
+    def store(self, variable_name):
+        self.code.append(f'STORE {variable_name}')
+
+    def operate(self, op):
+        if   op == '+': self.code.append('ADD')
+        elif op == '-': self.code.append('SUB')
+        elif op == '*': self.code.append('MUL')
+        elif op == '/': self.code.append('DIV')
+        elif op == '\\': self.code.append('DIV')
+        else:
+            self.abort(f"Operation {op} is not currently supported by the emitter")
+    
+    def call(self, function_name, *args):
+        arg_string = ', '.join(str(arg) for arg in args)
+        self.code.append(f'CALL {function_name}, {arg_string}')
+
+    def ret(self, value=None):
+        if value is not None:
+            self.code.append(f'RET {value}')
+        else:
+            self.code.append('RET')
